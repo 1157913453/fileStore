7372dda161a7ef1831cce22e8337bfda5998714d
天涯过客 <1157913453@qq.com>
2022-01-11 21:55:27 +0800

    改进文件类型判断逻辑

--- service/file_service/file.go
+++ service/file_service/file.go
@@ -19,57 +19,57 @@ var (
 func init() {
 	//longFileTypeMap.Store(strings.ToLower("D0CF11E0A1B11AE1000000000000000000000000000000003E000300FEFF090006000000000000000000000001000000"),"xls")
 	//longFileTypeMap.Store(strings.ToLower("D0CF11E0A1B11AE1000000000000000000000000000000003E000300FEFF090006000000000000000000000003000000"),"doc")
-	fileTypeMap.Store("ffd8ff", "jpg")                //JPEG (jpg)
-	fileTypeMap.Store("89504e47", "png")              //PNG (png)
-	fileTypeMap.Store("47494638", "gif")              //GIF (gif)
-	fileTypeMap.Store("49492a00", "tif")              //TIFF (tif)
-	fileTypeMap.Store("424d", "bmp")                  //16色位图(bmp)
-	fileTypeMap.Store("41433130", "dwg")              //CAD (dwg)
-	fileTypeMap.Store("3c21444f435459504520", "html") //HTML (html)   3c68746d6c3e0  3c68746d6c3e0
-	fileTypeMap.Store("3c68746d6c3e0", "html")        //HTML (html)   3c68746d6c3e0  3c68746d6c3e0
-	fileTypeMap.Store("3c21646f637479706520", "htm")  //HTM (htm)
-	fileTypeMap.Store("48544d4c207b0d0a0942", "css")  //css
-	fileTypeMap.Store("696b2e71623d696b2e71", "js")   //js
-	fileTypeMap.Store("7B5C727466315c", "rtf")        // 我（IBAS）猜想的rtf
-	fileTypeMap.Store("7b5c727466315c616e73", "rtf")  //Rich Text Format (rtf)
-	fileTypeMap.Store("38425053", "psd")              //Photoshop (psd)
-	fileTypeMap.Store("46726f6d3a203d3f6762", "eml")  //Email [Outlook Express 6] (eml)
-	fileTypeMap.Store("d0cf11e0a1b11ae10000", "doc")  //MS Excel 注意：word、msi 和 excel的文件头一样
-	fileTypeMap.Store("d0cf11e0a1b11ae10000", "vsd")  //Visio 绘图
-	fileTypeMap.Store("5374616E64617264204A", "mdb")  //MS Access (mdb)
-	fileTypeMap.Store("252150532D41646F6265", "ps")
-	fileTypeMap.Store("255044462d312e350d0a", "pdf")  //Adobe Acrobat (pdf)
-	fileTypeMap.Store("2e524d46000000120001", "rmvb") //rmvb/rm相同
-	fileTypeMap.Store("464c5601050000000900", "flv")  //flv与f4v相同
-	fileTypeMap.Store("00000020667479706d70", "mp4")
-	fileTypeMap.Store("49443303000000002176", "mp3")
-	fileTypeMap.Store("000001ba210001000180", "mpg") //
-	fileTypeMap.Store("3026b2758e66cf11a6d9", "wmv") //wmv与asf相同
-	fileTypeMap.Store("52494646e27807005741", "wav") //Wave (wav)
-	fileTypeMap.Store("52494646d07d60074156", "avi")
-	fileTypeMap.Store("4d546864000000060001", "mid") //MIDI (mid)
-	fileTypeMap.Store("504b03040a", "zip")           // 我（IBAS）看到的zip
+	fileTypeMap.Store("ffd8ff", "jpg")         //JPEG (jpg)
+	fileTypeMap.Store("89504e47", "png")       //PNG (png)
+	fileTypeMap.Store("47494638", "gif")       //GIF (gif)
+	fileTypeMap.Store("49492a00", "tif")       //TIFF (tif)
+	fileTypeMap.Store("424d", "bmp")           //16色位图(bmp)
+	fileTypeMap.Store("41433130", "dwg")       //CAD (dwg)
+	fileTypeMap.Store("3c21444f43545", "html") //HTML (html)   3c68746d6c3e0  3c68746d6c3e0
+	fileTypeMap.Store("3c68746d6c3e0", "html") //HTML (html)   3c68746d6c3e0  3c68746d6c3e0
+	fileTypeMap.Store("3c21646f63747", "htm")  //HTM (htm)
+	fileTypeMap.Store("48544d4c207b0", "css")  //css
+	fileTypeMap.Store("696b2e71623d6", "js")   //js
+	fileTypeMap.Store("7B5C727466315", "rtf")  // 我（IBAS）猜想的rtf
+	fileTypeMap.Store("7b5c727466315", "rtf")  //Rich Text Format (rtf)
+	fileTypeMap.Store("38425053", "psd")       //Photoshop (psd)
+	fileTypeMap.Store("46726f6d3a203d", "eml") //Email [Outlook Express 6] (eml)
+	fileTypeMap.Store("d0cf11e0a1b11a", "doc") //MS Excel 注意：word、msi 和 excel的文件头一样
+	fileTypeMap.Store("d0cf11e0a1b11a", "vsd") //Visio 绘图
+	fileTypeMap.Store("5374616E646172", "mdb") //MS Access (mdb)
+	fileTypeMap.Store("252150532D4164", "ps")
+	fileTypeMap.Store("255044462d312e", "pdf")  //Adobe Acrobat (pdf)
+	fileTypeMap.Store("2e524d46000000", "rmvb") //rmvb/rm相同
+	fileTypeMap.Store("464c5601050000", "flv")  //flv与f4v相同
+	fileTypeMap.Store("00000020667479", "mp4")
+	fileTypeMap.Store("49443303000000", "mp3")
+	fileTypeMap.Store("000001ba210001", "mpg") //
+	fileTypeMap.Store("3026b2758e66cf", "wmv") //wmv与asf相同
+	fileTypeMap.Store("52494646e27807", "wav") //Wave (wav)
+	fileTypeMap.Store("52494646d07d60", "avi")
+	fileTypeMap.Store("4d546864000000", "mid") //MIDI (mid)
+	fileTypeMap.Store("504b03040a", "zip")     // 我（IBAS）看到的zip
 	fileTypeMap.Store("504b030414", "zip")
-	fileTypeMap.Store("526172211a07009bd773", "rar") // 我（IBAS）看到的rar
-	fileTypeMap.Store("526172211a0700cf9073", "rar")
-	fileTypeMap.Store("235468697320636f6e66", "ini")
-	fileTypeMap.Store("504b0304140008080800", "jar") // 我（IBAS）看到的jar
-	fileTypeMap.Store("504b03040a0000000000", "jar")
-	fileTypeMap.Store("4d5a9000030000000400", "exe")        //可执行文件
-	fileTypeMap.Store("3c25402070616765206c", "jsp")        //jsp文件
-	fileTypeMap.Store("4d616e69666573742d56", "mf")         //MF文件
-	fileTypeMap.Store("3c3f786d6c2076657273", "xml")        //xml文件
-	fileTypeMap.Store("494e5345525420494e54", "sql")        //xml文件
-	fileTypeMap.Store("7061636b616765207765", "java")       //java文件
-	fileTypeMap.Store("406563686f206f66660d", "bat")        //bat文件
-	fileTypeMap.Store("1f8b0800000000000000", "gz")         //gz文件
-	fileTypeMap.Store("6c6f67346a2e726f6f74", "properties") //bat文件
-	fileTypeMap.Store("cafebabe0000002e0041", "class")      //bat文件
-	fileTypeMap.Store("49545346030000006000", "chm")        //bat文件
-	fileTypeMap.Store("04000000010000001300", "mxp")        //bat文件
-	fileTypeMap.Store("504b0304140006000800", "docx")       //docx文件
-	fileTypeMap.Store("d0cf11e0a1b11ae10000", "wps")        //WPS文字wps、表格et、演示dps都是一样的
-	fileTypeMap.Store("6431303a637265617465", "torrent")
+	fileTypeMap.Store("526172211a0700", "rar") // 我（IBAS）看到的rar
+	fileTypeMap.Store("526172211a0700", "rar")
+	fileTypeMap.Store("23546869732063", "ini")
+	fileTypeMap.Store("504b0304140008", "jar") // 我（IBAS）看到的jar
+	fileTypeMap.Store("504b03040a0000", "jar")
+	fileTypeMap.Store("4d5a9000030000", "exe")        //可执行文件
+	fileTypeMap.Store("3c254020706167", "jsp")        //jsp文件
+	fileTypeMap.Store("4d616e69666573", "mf")         //MF文件
+	fileTypeMap.Store("3c3f786d6c2076", "xml")        //xml文件
+	fileTypeMap.Store("494e5345525420", "sql")        //xml文件
+	fileTypeMap.Store("7061636b616765", "java")       //java文件
+	fileTypeMap.Store("406563686f206f", "bat")        //bat文件
+	fileTypeMap.Store("1f8b0800000000", "gz")         //gz文件
+	fileTypeMap.Store("6c6f67346a2e72", "properties") //bat文件
+	fileTypeMap.Store("cafebabe000000", "class")      //bat文件
+	fileTypeMap.Store("49545346030000", "chm")        //bat文件
+	fileTypeMap.Store("04000000010000", "mxp")        //bat文件
+	fileTypeMap.Store("504b0304140006", "docx")       //docx文件
+	fileTypeMap.Store("d0cf11e0a1b11a", "wps")        //WPS文字wps、表格et、演示dps都是一样的
+	fileTypeMap.Store("6431303a637265", "torrent")
 	fileTypeMap.Store("6D6F6F76", "mov")         //Quicktime (mov)
 	fileTypeMap.Store("FF575043", "wpd")         //WordPerfect (wpd)
 	fileTypeMap.Store("CFAD12FEC5FD746F", "dbx") //Outlook Express (dbx)
